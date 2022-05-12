4cf70b61688e99b60cbcebb4e6f488938a3c6594
MowKitty <supermowkitty@gmail.com>
2022-05-12 15:23:17 -0700

    Basic movement

--- src/main/java/com/example/chessgui/HelloController.java
+++ src/main/java/com/example/chessgui/HelloController.java
@@ -2,13 +2,21 @@ package com.example.chessgui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML private ImageView wPawn4;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onPawnClick(){
+        wPawn4.setY(wPawn4.getY()-50);
     }
 }
