d87c9dfde6bad15591bb21e14b136372434c5f68
MowKitty <supermowkitty@gmail.com>
2022-05-13 13:34:51 -0700

    Piece Selection + Knight movement

--- src/main/java/com/example/chessgui/HelloController.java
+++ src/main/java/com/example/chessgui/HelloController.java
@@ -3,12 +3,18 @@ package com.example.chessgui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.effect.ImageInput;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML private ImageView wPawn4;
+    @FXML private ImageView wRook1;
+    @FXML private ImageView wKnight1;
+    @FXML private ImageView wBishop1;
+    @FXML private ImageView wKing;
+    @FXML private ImageView wQueen;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -16,7 +22,49 @@ public class HelloController {
     }
 
     @FXML
-    protected void onPawnClick(){
-        wPawn4.setY(wPawn4.getY()-50);
+    protected void selectWP4(){
+        selectPiece(wPawn4);
+    }
+    @FXML
+    protected void selectWQ(){
+        selectPiece(wQueen);
+    }
+    @FXML
+    protected void selectWK(){
+        selectPiece(wKing);
+    }
+    @FXML
+    protected void selectWB1(){
+        selectPiece(wBishop1);
+    }
+    @FXML
+    protected void selectWKn1(){
+        selectPiece(wKnight1);
+    }
+    @FXML
+    protected void selectWR1(){
+        selectPiece(wRook1);
+    }
+
+    @FXML
+    protected void selectPiece(ImageView piece){
+        /*System.out.println(piece.getImage());
+        System.out.println(piece.getImage().getUrl());
+        System.out.println(piece.getImage().getUrl().contains("wPawn.png"));*/
+
+        if(piece.getImage().getUrl().contains("wPawn.png")){
+            pawnMove(piece);
+        }
+        else if(piece.getImage().getUrl().contains("wKnight.png")){
+            knightMove(piece);
+        }
+    }
+
+    private void pawnMove(ImageView pawn){
+        pawn.setY(pawn.getY() - 50);
+    }
+    private void knightMove(ImageView knight){
+        knight.setY(knight.getY()-100);
+        knight.setX(knight.getX()+50);
     }
 }
