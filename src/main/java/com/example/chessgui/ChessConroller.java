c2403328582979f51cdbbf2df7777e20e0d22ffa
MowKitty <supermowkitty@gmail.com>
2022-05-17 10:18:30 -0700

    Fixed Board Alignment and Updated Filenames

--- src/main/java/com/example/chessgui/ChessConroller.java
+++ src/main/java/com/example/chessgui/ChessConroller.java
@@ -2,11 +2,9 @@ package com.example.chessgui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.effect.ImageInput;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class HelloController {
+public class ChessConroller {
     @FXML
     private Label welcomeText;
     @FXML private ImageView wPawn4;
