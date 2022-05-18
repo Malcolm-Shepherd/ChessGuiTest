2f3e8ea735f0ed738a1efdf20999bea3e441722b
MowKitty <supermowkitty@gmail.com>
2022-05-18 10:49:31 -0700

    Changed Board to Gridpane

--- src/main/java/com/example/chessgui/ChessConroller.java
+++ src/main/java/com/example/chessgui/ChessConroller.java
@@ -1,18 +1,24 @@
 package com.example.chessgui;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 public class ChessConroller {
     @FXML
     private Label welcomeText;
+    @FXML private GridPane Board;
+    @FXML private Button button;
     @FXML private ImageView wPawn4;
     @FXML private ImageView wRook1;
     @FXML private ImageView wKnight1;
     @FXML private ImageView wBishop1;
     @FXML private ImageView wKing;
     @FXML private ImageView wQueen;
+    @FXML private ImageView selectedPiece;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -49,6 +55,7 @@ public class ChessConroller {
         /*System.out.println(piece.getImage());
         System.out.println(piece.getImage().getUrl());
         System.out.println(piece.getImage().getUrl().contains("wPawn.png"));*/
+        selectedPiece = piece;
 
         if(piece.getImage().getUrl().contains("wPawn.png")){
             pawnMove(piece);
@@ -62,7 +69,25 @@ public class ChessConroller {
         pawn.setY(pawn.getY() - 50);
     }
     private void knightMove(ImageView knight){
-        knight.setY(knight.getY()-100);
-        knight.setX(knight.getX()+50);
+        /*System.out.println(Board.getColumnIndex(knight));
+        System.out.println(Board.getRowIndex(knight));*/
+        createMoveButton(Board.getColumnIndex(knight)+1, Board.getRowIndex(knight)-2);
+        /*Board.setColumnIndex(knight, Board.getColumnIndex(knight)-1);
+        Board.setRowIndex(knight, Board.getRowIndex(knight)-2);*/
+    }
+
+    private void createMoveButton(int col, int row){
+        Button moveButton = new Button();
+        moveButton.setMinHeight(50);
+        moveButton.setMinWidth(50);
+        moveButton.setOnMouseClicked(movePiece());
+        Board.add(moveButton, col, row);
+    }
+
+    @FXML
+    private EventHandler movePiece(){
+        Board.setColumnIndex(selectedPiece, Board.getColumnIndex(selectedPiece)+1);
+        Board.setRowIndex(selectedPiece, Board.getRowIndex(selectedPiece)-2);
+        return null;
     }
 }
